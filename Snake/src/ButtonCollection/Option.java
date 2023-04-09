746cc055c7751c530df2acd95e4ff28c7c3203ee
ershimiyangguang <2740929986@qq.com>
2023-04-09 12:46:48 +0800

    bc

--- Snake/src/ButtonCollection/Option.java
+++ Snake/src/ButtonCollection/Option.java
@@ -1,9 +1,13 @@
 package ButtonCollection;
 
+import Main.OptionFrame;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Option extends Button implements ActionListener {
+
+    OptionFrame optionframe = new OptionFrame();
     public Option() {
         super.setText("设置(O)");
         super.setWidth(6);
@@ -13,6 +17,6 @@ public class Option extends Button implements ActionListener {
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        optionframe.setVisible(true);
     }
 }
