61e931815599c06adb95263023f63c4b5b597bb0
lkl <2440552281@qq.com>
2023-05-22 10:21:01 +0800

    Merge remote-tracking branch 'origin/feature_emery' into feature_emery
    # Conflicts:
    #	Snake/src/Main/GameFrame.java

--- Snake/src/Object/Enemy.java
+++ Snake/src/Object/Enemy.java
@@ -11,23 +11,43 @@ public class Enemy {
     public enum Enemystate{attack,escape,wait}   //敌人的状态
     public Enemystate enemystate;
     boolean  attack_direction; //攻击的方向
-    int escape_direction;  //逃跑的方向
+    int escape_direction,range,distance;  //逃跑的方向
     Location attack_range []= new Location[5];
-
-    private int damage;
-    //伤害
-    //函数
+    static int count;
+    static int i;
+    static  void add(int x) {
+        count=count+x;
+    }
 
     public Enemy() {
         init();
     }
     //重置敌人位置与伤害
     public void init() {
+        range=8;
         Random r =new Random();
-        location.setX(r.nextInt(31));
-        location.setY(r.nextInt(21));   //随机生成敌人的位置
-        this.damage=2;
-        this.enemystate=Enemystate.attack;
+        int i=r.nextInt(3);
+        if(i==0) {
+            location.setX(0);
+            location.setY(r.nextInt(21));   //随机生成敌人的位置
+            escape_direction=1;
+        }
+        else if(i==1) {
+            location.setX(30);
+            location.setY(r.nextInt(21));   //随机生成敌人的位置
+            escape_direction=-1;
+        }
+        else if(i==2) {
+            location.setX(r.nextInt(31));
+            location.setY(0);   //随机生成敌人的位置
+            escape_direction=2;
+        }
+        else {
+            location.setX(r.nextInt(31));
+            location.setY(20);   //随机生成敌人的位置
+            escape_direction=-2;
+        }
+         //随机生成敌人的位置
 
 
     }
@@ -35,18 +55,25 @@ public class Enemy {
     public void attack(Snake snake, GameFrame frame) {
         if(enemystate==Enemystate.attack) {    //当敌人状态为攻击时，判断敌人是否攻击到蛇//
             for (int i = 0; i < snake.getLength(); i++) {
-                if (detection(location,snake)) {
-                    snake.setLength(snake.getLength() - 1);//如果攻击到蛇，则蛇的长度-1，并且敌人的状态改为逃跑
-                    enemystate =Enemystate. escape;
-                    break;
+                if (location.equals(snake.getSnake_body()[i])) {
+                    ;//如果攻击到蛇，则蛇的长度-1，并且敌人的状态改为逃跑
+                    if(i==0){
+                       // return 2;
+                    }
+                    else {
+
+                        init();
+                        break;
+                        //return 1;
+                    }
+
+
 
                 }
             }
 
         }
-        else if(enemystate==Enemystate.escape){
 
-        }
 
     }
     //移动
@@ -54,8 +81,16 @@ public class Enemy {
         Location location1[] = new Location[1000];
         Location head = new Location();
         location1 = snake.getSnake_body();
+        distance=Math.abs(location.getY()-location1[0].getY())+Math.abs(location.getX()-location1[0].getX());
         attack_direction = new Random().nextBoolean();
-        if (enemystate ==Enemystate. attack) {//如果敌人状态为攻击，则敌人根据蛇的位置自动改变自己的位置
+        if(distance<=range){
+            enemystate=Enemystate.attack;
+        }
+        else{
+            enemystate=Enemystate.wait;
+        }
+        if (enemystate ==Enemystate. attack) {//如果敌人状态为攻击，则敌人根据蛇的位置自动改变自己的位
+
             if (location1[0].getX() > location.getX() && location1[0].getY() > location.getY()) {
                 if (attack_direction)
                 {
@@ -102,36 +137,39 @@ public class Enemy {
             }
 
         }
-        else if(enemystate==Enemystate.escape){    //如果敌人状态逃跑
-            if(escape_direction==1)
-            {
-                location.setX(location.getX()-1);
 
-            }
-            else if(escape_direction==-1){
-                location.setX(location.getX()+1);
-            }
-            else if(escape_direction==2)
-            {
-                location.setY(location.getY()-1);
+        else if(enemystate==Enemystate.wait){
+
+            if(distance>=range) {
+                if(location.getX()<0||location.getX()>31||location.getY()<0||location.getY()>21||i>=2*range){
+                    escape_direction=-escape_direction;
+                    i=0;
+                }
+
+                if (escape_direction == 1) {
+                    i++;
+                    location.setX(location.getX() + 1);
+                }
+                else if (escape_direction == -1) {
+                    i++;
+                    location.setX(location.getX() - 1);
+                }
+                else if (escape_direction == 2) {
+                    i++;
+                    location.setY(location.getY() + 1);
+
+                }
+                else if(escape_direction==-2){
+                    location.setY(location.getY() - 1);
+                }
             }
             else{
-                location.setY(location.getY()+1);
-
-            }
-
-        }
-        else{
-            Location location2[] = new Location[100];
-            location2 = snake.getSnake_body();
-            Rectangle r = new Rectangle((location.getX()-4)*30,(location.getY()-4),30,30);
-            if(r.contains(location2[0].getX(),location2[0].getY())){
                 enemystate=Enemystate.attack;
             }
         }
 
     }
-    public boolean detection (Location enemy,Snake snake) {
+    /*public boolean detection (Location enemy,Snake snake) {
         Location location2[]= new Location[1000];
         location2=snake.getSnake_body();
         if (enemystate == Enemystate.attack) {
@@ -156,29 +194,22 @@ public class Enemy {
 
 
         }
-        else if(enemystate==Enemystate.escape){
-            for(int i=0;i<snake.getLength();i++){
-                if(location2[i].equals(enemy))
-                    return true;
-            }
 
-        }
         return false;
 
-    }
+    }*/
 
     public void paint(Graphics gImage, int x, int y, int l, Snake snake) {
-        if (location.getX() >= 0 && location.getX() <= 31 && location.getY() >= 0 && location.getY() <= 21) {
-            gImage.setColor(Color.BLACK);
-            gImage.fillRect((location.getX() * l) + x, (location.getY() * l + y), l, l);
-        }
+
 
         if (enemystate == Enemystate.attack) {
-            for (int i = 1; i <=4; i++) {
-                gImage.setColor(Color.yellow);
-                gImage.fillRect((attack_range[i].getX()*l)+x,(attack_range[i].getY()*l)+y,l,l);
-            }
+            gImage.setColor(Color.BLACK);
+            gImage.fillRect((location.getX() * l) + x, (location.getY() * l + y), l, l);
 
+        }
+        if(enemystate==Enemystate.wait){
+            gImage.setColor(Color.CYAN);
+            gImage.fillRect((location.getX() * l) + x, (location.getY() * l + y), l, l);
 
         }
 
